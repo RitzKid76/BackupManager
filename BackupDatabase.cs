e76fa41b58a4092454ec0e3871e85904a32d7643
RitzKid76 <tjbaker2003@hotmail.com>
2026-08-15 18:07:59 -0400

    remove redundant class

--- BackupDatabase.cs
+++ BackupDatabase.cs
@@ -6,6 +6,8 @@ namespace Backup;
 
 public static class BackupDatabase
 {
+    private static readonly Queue<ObjectReference> rollbackQueue = [];
+
     public static void Generate(string? backupName = null, bool force = false)
     {
         if (
@@ -50,6 +52,45 @@ public static class BackupDatabase
                 return true;
 
         return false;
+    }
+
+    public static void Rollback(BackupEntry backup)
+    {
+        foreach (ObjectReference reference in backup.References)
+            rollbackQueue.Enqueue(reference);
+
+        Rollback();
+    }
+
+    private static void EnqueueTree(Tree tree)
+    {
+        foreach (ObjectReference reference in tree.References)
+        {
+            reference.PrependPath(tree.Name);
+            rollbackQueue.Enqueue(reference);
+        }
+    }
+
+    private static void Rollback()
+    {
+        while (rollbackQueue.TryDequeue(out ObjectReference? reference))
+        {
+            switch (reference.Format)
+            {
+                case ObjectFormat.TREE:
+                    HandleTreeRollback(reference);
+                    break;
+                case ObjectFormat.BLOB:
+                    Database.ReadFile(reference);
+                    break;
+            }
+        }
+    }
+
+    private static void HandleTreeRollback(ObjectReference treeReference)
+    {
+        Tree tree = Database.ReadTree(treeReference);
+        EnqueueTree(tree);
     }
 
     private static string BackupName() =>
