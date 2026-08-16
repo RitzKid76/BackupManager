9b1d91b074cd91d3cd35b12857345767237e84ce
RitzKid76 <tjbaker2003@hotmail.com>
2026-08-16 13:12:00 -0400

    add continuous mode

--- BackupController.cs
+++ BackupController.cs
@@ -1,4 +1,6 @@
-﻿using Backup.Commands.Core;
+﻿using System.Text.RegularExpressions;
+using Backup.BackupComponents;
+using Backup.Commands.Core;
 
 namespace Backup;
 
@@ -6,6 +8,34 @@ public static class BackupController
 {
     public static void Main(string[] args)
     {
-        CommandExecutor.Execute(args);
+        if (args.Length == 0)
+            ContinuousMode();
+        else
+            CommandExecutor.Execute(args);
+    }
+
+    private static void ContinuousMode()
+    {
+        BackupLogger.Log("started continuous mode\nrun 'help' for options, and 'exit' to exit");
+
+        const string tokenPattern = @"[^\s""']+|""([^""]*)""|'([^']*)'";
+
+        while (true)
+        {
+            Console.Write("> ");
+            string? input = Console.ReadLine();
+
+            if (input is null)
+                continue;
+
+            if (input == "exit")
+                return;
+
+            string[] args = Regex.Matches(input, tokenPattern)
+                .Select(m => m.Value)
+                .ToArray();
+
+            CommandExecutor.Execute(args);
+        }
     }
 }
